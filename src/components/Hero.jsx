3eb423be25bd2a55f5d68b9f99a219a144a06bb0
nadeem <nadeemwazir0123@gmail.com>
2025-03-17 15:55:40 +0500

    changes in hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import Profile from '../assets/Profile.jpg'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
   return (
     <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
-        <div className='flex flex-wrap'>
-            <div className='w-full lg:w-1/2'>
-            <div className='flex flex-col items-center lg:items-start'>
-                <motion.h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16'>
-                    Muhammad Nadeem
-                </motion.h1>
-                <span className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-4xl rounded-md'>Odoo Technical Developer and Frontend Developer</span>
-<<<<<<< HEAD
-                <p className='mt-3'>As a skilled odoo Technical developer and frontend,<p> I can customize a module from scratch and can make changes in existing module</p>. specialize in building responsive and user-friendly web interfaces with expertise in HTML, CSS, JavaScript, and modern frameworks like React. I am passionate about creating seamless user experiences and have a strong eye for design and detail, ensuring every project is visually engaging and functionally robust.</p>
-=======
-                <p className='mt-3'>As a skilled frontend  and odoo developer,<p> I can customize a module from scratch and can make changes in existing module</p>. specialize in building responsive and user-friendly web interfaces with expertise in HTML, CSS, JavaScript, and modern frameworks like React. I am passionate about creating seamless user experiences and have a strong eye for design and detail, ensuring every project is visually engaging and functionally robust.</p>
->>>>>>> changes in hero components
-            </div>
-            </div>
-            <div className='w-full lg:w-1/2 lg:p-8 '>
-            <div className='flex justify-center'>
-                <img className='h-64 lg:h-80 ml-20 mt-10 rounded-full' src={Profile} alt="M Nadeem" />
-                </div>
-            </div>
+      <div className='flex flex-wrap'>
+        <div className='w-full lg:w-1/2'>
+          <div className='flex flex-col items-center lg:items-start'>
+            <motion.h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16'>
+              Muhammad Nadeem
+            </motion.h1>
+            <span className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-4xl rounded-md'>
+              Odoo Developer and FrontEnd
+            </span>
+            <p className='mt-3'>
+              As a skilled frontend and Odoo developer, I can customize a module from scratch and make changes to existing modules.
+            </p>
+            <p>
+              I specialize in building responsive and user-friendly web interfaces with expertise in HTML, CSS, JavaScript, and modern frameworks like React. I am passionate about creating seamless user experiences and have a strong eye for design and detail, ensuring every project is visually engaging and functionally robust.
+            </p>
+          </div>
         </div>
-
+        <div className='w-full lg:w-1/2 lg:p-8'>
+          <div className='flex justify-center'>
+            <img className='h-64 lg:h-80 ml-20 mt-10 rounded-full' src={Profile} alt="M Nadeem" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
+
